53213397b8bf4c4e327f3fec2d16623be6b1f9c0
Stephan Wenger <wenger@cg.cs.tu-bs.de>
2014-03-24 15:43:38 +0100

    add nonnegative l1 norm

--- convexopt/operators/l1norm.py
+++ convexopt/operators/l1norm.py
@@ -5,7 +5,7 @@ import numpy as _np
 
 from convexopt.operators.util import Operator
 
-__all__ = ["L1Norm"]
+__all__ = ["L1Norm", "NonnegativeL1Norm"]
 
 
 class L1NormGradient(Operator):
@@ -29,3 +29,26 @@ class L1Norm(Operator):
 
     def __call__(self, x):
         return _np.linalg.norm(x.ravel(), 1)
+
+
+class NonnegativeL1NormGradient(Operator):
+    """Gradient of l1-norm
+
+    The actual subgradient would be multi-valued at zero, which is not
+    currently supported.  However, the backward operator, the proximal mapping
+    of the l1-norm, is implemented (as soft-thresholding).
+    """
+
+    def backward(self, x, tau):
+        return _np.maximum(x - tau, 0)
+
+
+class NonnegativeL1Norm(Operator):
+    """l1-norm ||x||_1
+    """
+
+    shape = (0, None)
+    gradient = NonnegativeL1NormGradient()
+
+    def __call__(self, x):
+        return _np.linalg.norm(x.ravel(), 1) if all(x.ravel() >= 0) else float("inf")
